af4339cdb281694fb485ae31cd8d03ed05bb9470
irsyaduddin123 <jazim287@gmail.com>
2024-01-08 00:07:59 +0700

    perubahan auto box

--- includes/isi.php
+++ includes/isi.php
@@ -1,10 +1,17 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper " class="d-flex flex-column">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+
+    <!-- isi Content -->
 <div class="row">
+<!-- box 1 -->
         <div class="col-xs-3">
           <div class="box">
             <div class="box-body">
-              <div id="grafik1" style="height: 300px">
+              <div id="grafik1" style="height: auto">
                 <div class="card-header py-3">
-                <h3 class="m-0 font-weight-bold text-primary">
+                <h4 class="m-0 font-bold text-primary text-center uppercase">
                   <?php
                   $conn = new mysqli('localhost', 'root', '', 'ppsi');
                     // Check the connection
@@ -26,8 +33,7 @@
                     // Close the connection
                     $conn->close();
                     ?>
-                </h3>
-
+                </h4>
 
                 </div>
                 <div class="card-body">
@@ -40,7 +46,7 @@
                       $nama_pasar1 = $conn->query($sql1);
                       $harga1 = $conn->query($sql1);
                     ?>
-                        <canvas class="my-4 w-100" id="myLineChart1" width="900" height="380"></canvas>
+                        <canvas class="my-4 w-100" id="myLineChart1" width="900" height="500"></canvas>
                         
                       </div>
                   </div>
@@ -49,12 +55,35 @@
           </div>
         </div>
          
+        <!-- box 2 -->
         <div class="col-xs-3">
           <div class="box">
             <div class="box-body">
-              <div id="grafik1" style="height: 300px">
+              <div id="grafik1" style="height: auto">
                 <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Grafik 2</h6>
+                <h4 class="m-0 font-bold text-primary text-center uppercase">
+                  <?php
+                  $conn = new mysqli('localhost', 'root', '', 'ppsi');
+                    // Check the connection
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                    }
+                  
+                    $sqlnm = "SELECT nama FROM barang WHERE id = 2";
+                    $result = $conn->query($sqlnm);
+                  
+                    if ($result->num_rows > 0) {
+                        // Assuming you expect only one row, you can use fetch_assoc()
+                        $row = $result->fetch_assoc();
+                        echo $row['nama'];
+                    } else {
+                        echo "No results found";
+                    }
+                  
+                    // Close the connection
+                    $conn->close();
+                    ?>
+                </h4>
                 </div>
                 <div class="card-body">
                     <div class="chart-bar text-center small">
@@ -66,7 +95,7 @@
                   $nama_pasar2= $conn->query($sql2);
                   $harga2 = $conn->query($sql2);
                   ?>
-                       <canvas id="myLineChart2"></canvas>
+                       <canvas class="my-4 w-100" id="myLineChart2" width="900" height="500"></canvas>
                         
                       </div>
                   </div>
@@ -74,12 +103,37 @@
             </div>
           </div>
         </div>
+
+        <!-- box 3 -->
+
         <div class="col-xs-3">
           <div class="box">
             <div class="box-body">
-              <div id="grafik1" style="height: 300px">
+              <div id="grafik1" style="height: auto">
                 <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Grafik 3</h6>
+                <h4 class="m-0 font-bold text-primary text-center uppercase">
+                  <?php
+                  $conn = new mysqli('localhost', 'root', '', 'ppsi');
+                    // Check the connection
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                    }
+                  
+                    $sqlnm = "SELECT nama FROM barang WHERE id = 3";
+                    $result = $conn->query($sqlnm);
+                  
+                    if ($result->num_rows > 0) {
+                        // Assuming you expect only one row, you can use fetch_assoc()
+                        $row = $result->fetch_assoc();
+                        echo $row['nama'];
+                    } else {
+                        echo "No results found";
+                    }
+                  
+                    // Close the connection
+                    $conn->close();
+                    ?>
+                </h4>
                 </div>
                 <div class="card-body">
                     <div class="chart-bar">
@@ -91,7 +145,7 @@
                   $nama_pasar3 = $conn->query($sql3);
                   $harga3 = $conn->query($sql3);
                   ?>
-                    <canvas class="my-4 w-100" id="myLineChart3" width="900" height="380"></canvas>
+                    <canvas class="my-4 w-100" id="myLineChart3" width="900" height="500"></canvas>
                        
                       </div>
                   </div>
@@ -99,24 +153,50 @@
             </div>
           </div>
         </div>
+
+        <!-- box 4 -->
         <div class="col-xs-3">
           <div class="box">
             <div class="box-body">
-              <div id="grafik1" style="height: 300px">
+              <div id="grafik1" style="height: auto">
                 <div class="card-header py-3">
+                <h4 class="m-0 font-bold text-primary text-center uppercase">
                   <?php
                   $conn = new mysqli('localhost', 'root', '', 'ppsi');
-                  // include koneksi.php
-                  $sql4 = "SELECT a.id, b.nama AS nama_barang, p.nama AS nama_pasar, a.harga, a.tanggal_input FROM harga_barang a JOIN barang b ON a.barang_id=b.id JOIN pasar p ON a.pasar_id=p.id WHERE a.barang_id = '4'";
-                  $nama_barang4 = $conn->query($sql4);
-                  $nama_pasar4 = $conn->query($sql4);
-                  $harga4 = $conn->query($sql4);
-                  ?>
-                    <h6 class="m-0 font-weight-bold text-primary">Grafik 4</h6>
+                    // Check the connection
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                    }
+                  
+                    $sqlnm = "SELECT nama FROM barang WHERE id = 4";
+                    $result = $conn->query($sqlnm);
+                  
+                    if ($result->num_rows > 0) {
+                        // Assuming you expect only one row, you can use fetch_assoc()
+                        $row = $result->fetch_assoc();
+                        echo $row['nama'];
+                    } else {
+                        echo "No results found";
+                    }
+                  
+                    // Close the connection
+                    $conn->close();
+                    ?>
+                </h4>
+                  
+                    
                 </div>
                 <div class="card-body">
-                    <div class="chart-bar text-center small">
-                        <canvas id="myLineChart4"></canvas>
+                    <div class="chart-bar">
+                      <?php
+                        $conn = new mysqli('localhost', 'root', '', 'ppsi');
+                        // include koneksi.php
+                        $sql4 = "SELECT a.id, b.nama AS nama_barang, p.nama AS nama_pasar, a.harga, a.tanggal_input FROM harga_barang a JOIN barang b ON a.barang_id=b.id JOIN pasar p ON a.pasar_id=p.id WHERE a.barang_id = '4'";
+                        $nama_barang4 = $conn->query($sql4);
+                        $nama_pasar4 = $conn->query($sql4);
+                        $harga4 = $conn->query($sql4);
+                      ?>
+                        <canvas class="my-4 w-100" id="myLineChart4" width="900" height="500"></canvas>
                        
                       </div>
                   </div>
@@ -124,15 +204,39 @@
             </div>
           </div>
         </div>
+
+        <!-- box 5 -->
         <div class="col-xs-3">
           <div class="box">
             <div class="box-body">
-              <div id="grafik1" style="height: 300px">
+              <div id="grafik1" style="height: auto">
                 <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Grafik 5</h6>
+                <h4 class="m-0 font-bold text-primary text-center uppercase">
+                  <?php
+                  $conn = new mysqli('localhost', 'root', '', 'ppsi');
+                    // Check the connection
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                    }
+                  
+                    $sqlnm = "SELECT nama FROM barang WHERE id = 5";
+                    $result = $conn->query($sqlnm);
+                  
+                    if ($result->num_rows > 0) {
+                        // Assuming you expect only one row, you can use fetch_assoc()
+                        $row = $result->fetch_assoc();
+                        echo $row['nama'];
+                    } else {
+                        echo "No results found";
+                    }
+                  
+                    // Close the connection
+                    $conn->close();
+                    ?>
+                </h4>
                 </div>
                 <div class="card-body">
-                    <div class="chart-bar text-center small">
+                    <div class="chart-bar ">
                     <?php
                       $conn = new mysqli('localhost', 'root', '', 'ppsi');
                       // include koneksi.php
@@ -141,7 +245,7 @@
                       $nama_pasar5 = $conn->query($sql5);
                       $harga5 = $conn->query($sql5);
                     ?>
-                        <canvas id="myLineChart5"></canvas>
+                        <canvas class="my-4 w-100" id="myLineChart5" width="900" height="500"></canvas>
                        
                       </div>
                   </div>
@@ -149,15 +253,39 @@
             </div>
           </div>
         </div>
+
+        <!-- box 6 -->
         <div class="col-xs-3">
           <div class="box">
             <div class="box-body">
-              <div id="grafik1" style="height: 300px">
+              <div id="grafik1" style="height: auto">
                 <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Grafik 6</h6>
+                <h4 class="m-0 font-bold text-primary text-center uppercase">
+                  <?php
+                  $conn = new mysqli('localhost', 'root', '', 'ppsi');
+                    // Check the connection
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                    }
+                  
+                    $sqlnm = "SELECT nama FROM barang WHERE id = 6";
+                    $result = $conn->query($sqlnm);
+                  
+                    if ($result->num_rows > 0) {
+                        // Assuming you expect only one row, you can use fetch_assoc()
+                        $row = $result->fetch_assoc();
+                        echo $row['nama'];
+                    } else {
+                        echo "No results found";
+                    }
+                  
+                    // Close the connection
+                    $conn->close();
+                    ?>
+                </h4>
                 </div>
                 <div class="card-body">
-                    <div class="chart-bar text-center small">
+                    <div class="chart-bar ">
                     <?php
                       $conn = new mysqli('localhost', 'root', '', 'ppsi');
                       // include koneksi.php
@@ -166,7 +294,7 @@
                       $nama_pasar6 = $conn->query($sql6);
                       $harga6 = $conn->query($sql6);
                     ?>
-                        <canvas id="myLineChart6"></canvas>
+                        <canvas class="my-4 w-100" id="myLineChart6" width="900" height="500"></canvas>
                        
                       </div>
                   </div>
@@ -174,24 +302,48 @@
             </div>
           </div>
         </div>
+
+        <!-- box 7 -->
         <div class="col-xs-3">
           <div class="box">
             <div class="box-body">
-              <div id="grafik1" style="height: 300px">
+              <div id="grafik1" style="height: auto">
                 <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Grafik 7</h6>
+                <h4 class="m-0 font-bold text-primary text-center uppercase">
+                  <?php
+                  $conn = new mysqli('localhost', 'root', '', 'ppsi');
+                    // Check the connection
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                    }
+                  
+                    $sqlnm = "SELECT nama FROM barang WHERE id = 7";
+                    $result = $conn->query($sqlnm);
+                  
+                    if ($result->num_rows > 0) {
+                        // Assuming you expect only one row, you can use fetch_assoc()
+                        $row = $result->fetch_assoc();
+                        echo $row['nama'];
+                    } else {
+                        echo "No results found";
+                    }
+                  
+                    // Close the connection
+                    $conn->close();
+                    ?>
+                </h4>
                 </div>
                 <div class="card-body">
                     <div class="chart-bar text-center small">
                     <?php
                       $conn = new mysqli('localhost', 'root', '', 'ppsi');
                       // include koneksi.php
-                      $sql6 = "SELECT a.id, b.nama AS nama_barang, p.nama AS nama_pasar, a.harga, a.tanggal_input FROM harga_barang a JOIN barang b ON a.barang_id=b.id JOIN pasar p ON a.pasar_id=p.id WHERE a.barang_id = '6'";
+                      $sql6 = "SELECT a.id, b.nama AS nama_barang, p.nama AS nama_pasar, a.harga, a.tanggal_input FROM harga_barang a JOIN barang b ON a.barang_id=b.id JOIN pasar p ON a.pasar_id=p.id WHERE a.barang_id = '7'";
                       $nama_barang6 = $conn->query($sql6);
                       $nama_pasar6 = $conn->query($sql6);
                       $harga6 = $conn->query($sql6);
                     ?>
-                        <canvas id="myLineChart6"></canvas>
+                        <canvas class="my-4 w-100" id="myLineChart7" width="900" height="500"></canvas>
                        
                       </div>
                   </div>
@@ -199,38 +351,59 @@
             </div>
           </div>
         </div>
+
+        <!-- box 8 -->
         <div class="col-xs-3">
           <div class="box">
             <div class="box-body">
-              <div id="grafik1" style="height: 300px">
+              <div id="grafik1" style="height: auto">
                 <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Grafik 8</h6>
+                <h4 class="m-0 font-bold text-primary text-center uppercase">
+                  <?php
+                  $conn = new mysqli('localhost', 'root', '', 'ppsi');
+                    // Check the connection
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                    }
+                  
+                    $sqlnm = "SELECT nama FROM barang WHERE id = 8";
+                    $result = $conn->query($sqlnm);
+                  
+                    if ($result->num_rows > 0) {
+                        // Assuming you expect only one row, you can use fetch_assoc()
+                        $row = $result->fetch_assoc();
+                        echo $row['nama'];
+                    } else {
+                        echo "No results found";
+                    }
+                  
+                    // Close the connection
+                    $conn->close();
+                    ?>
+                </h4>
                 </div>
                 <div class="card-body">
                     <div class="chart-bar text-center small">
                     <?php
                       $conn = new mysqli('localhost', 'root', '', 'ppsi');
                       // include koneksi.php
-                      $sql6 = "SELECT a.id, b.nama AS nama_barang, p.nama AS nama_pasar, a.harga, a.tanggal_input FROM harga_barang a JOIN barang b ON a.barang_id=b.id JOIN pasar p ON a.pasar_id=p.id WHERE a.barang_id = '6'";
+                      $sql6 = "SELECT a.id, b.nama AS nama_barang, p.nama AS nama_pasar, a.harga, a.tanggal_input FROM harga_barang a JOIN barang b ON a.barang_id=b.id JOIN pasar p ON a.pasar_id=p.id WHERE a.barang_id = '8'";
                       $nama_barang6 = $conn->query($sql6);
                       $nama_pasar6 = $conn->query($sql6);
                       $harga6 = $conn->query($sql6);
                     ?>
-                        <canvas id="myLineChart6"></canvas>
-                       
+                        <canvas class="my-4 w-100" id="myLineChart8" width="900" height="500"></canvas>
                       </div>
                   </div>
                 </div>
             </div>
           </div>
         </div>
-          
       </div>
       
 
-
     
-
+<!-- KUMPULAN script -->
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eI7PSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBxD+l6sEG4zoLp" crossorigin="anonymous"></script>
     <!-- <script src="dashboard.js"></script>
